d78dec5bb8223981d28cb226ef64dd7257763811
Davide Branco <davidegabrielbranco@hotmail.com>
2020-07-10 16:18:26 +0000

    added constructor and clear methods to GFX_Engine class

--- Engine/GFX.js
+++ Engine/GFX.js
@@ -10,4 +10,19 @@ class GFX_Engine {
         purple: "#C217C2",
         white: "#000000" //change later because this is actually black
     }
+
+    constructor() {
+        this.measures = { padding: 5, size: 37, border: 2}
+        this.borderColor = GFX_Engine.colors.black;
+
+        let canvas = document.createElement("canvas");
+        this.context = canvas.getContext("2d");
+        canvas.height = window.innerHeight;
+        canvas.width = (this.measures.size - this.measures.border) * 10 + this.measures.border + this.measures.padding * 2;
+        document.getElementById("lab").append(canvas);
+    }
+
+    clear() {
+        this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height)
+    }
 }
