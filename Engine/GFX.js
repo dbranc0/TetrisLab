8a51be9a72cc17008e1df720817cf97fcb1bc4c9
Davide Branco <davidegabrielbranco@hotmail.com>
2020-07-10 16:16:43 +0000

    created GFX_Engine class with a static property colors

---
+++ Engine/GFX.js
@@ -0,0 +1,13 @@
+class GFX_Engine {
+    static colors = {
+        black: "#555555", //change black and white in code later
+        blue: "#2812ee",
+        orange: "#f76d0d",
+        red: "#e60026",
+        green: "#66e319",
+        yellow: "#eeff00",
+        cyan: "#00FFFF",
+        purple: "#C217C2",
+        white: "#000000" //change later because this is actually black
+    }
+}
